b385f18f72d4b805ecbe6d70f9282bbe1a979d20
Jordan Bean <jordanbean@microsoft.com>
2025-06-26 17:08:12 -0500

    mi

--- infra/modules/api_management/policies.tf
+++ infra/modules/api_management/policies.tf
@@ -4,7 +4,6 @@ resource "azurerm_api_management_api_policy" "openai_api_policy" {
   api_name            = azurerm_api_management_api.openai_api.name
   xml_content         = file("${path.module}/policies/openai.xml")
   depends_on = [
-    azurerm_api_management_policy_fragment.get_access_token_to_openai_policy,
     azurerm_api_management_policy_fragment.openai_event_hub_logging_inbound_policy,
     azurerm_api_management_policy_fragment.openai_event_hub_logging_outbound_policy,
     azurerm_api_management_policy_fragment.setup_correlation_id_policy,
@@ -55,12 +54,4 @@ resource "azurerm_api_management_policy_fragment" "openai_event_hub_logging_outb
     azurerm_api_management_named_value.user_assigned_identity_client_id,
     azurerm_api_management_logger.event_hub_logger
   ]
-}
-
-resource "azurerm_api_management_policy_fragment" "get_access_token_to_openai_policy" {
-  api_management_id = azapi_resource.api_management.id
-  name              = "get-access-token-to-openai"
-  value             = file("${path.module}/policies/get-access-token-to-openai.xml")
-  format            = "rawxml"
-  depends_on        = [azurerm_api_management_named_value.user_assigned_identity_client_id]
 }
