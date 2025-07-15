b062fe071efe9988cb6ee3884d25b1856d268c11
Jordan Bean <jordanbean@microsoft.com>
2025-07-15 18:05:11 -0500

    apim

--- infra/modules/api_management/main.tf
+++ infra/modules/api_management/main.tf
@@ -13,6 +13,35 @@ data "azapi_resource" "resource_group" {
   name = var.resource_group_name
 }
 
+# module "api_management" {
+#   source              = "Azure/avm-res-apimanagement-service/azurerm"
+#   version             = "0.0.4"
+#   name                = azurecaf_name.api_management_name.result
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+#   publisher_email     = var.publisher_email
+#   publisher_name      = var.publisher_name
+#   sku_name            = "${var.sku_name}_${var.sku_capacity}"
+#   tags                = var.tags
+#   # diagnostic_settings = {
+#   #   default = {
+#   #     workspace_resource_id = var.log_analytics_workspace_id
+#   #   }
+#   # }
+#   managed_identities = {
+#     user_assigned_identity_id = [var.user_assigned_identity_id]
+#   }
+#   private_endpoints = {
+#     api_management_private_endpoint = {
+#       subnet_resource_id = var.private_endpoint_subnet_id
+#     }
+#   }
+#   public_network_access_enabled = false
+#   #virtual_network_subnet_id     = var.api_management_subnet_id
+#   #virtual_network_type          = "None"
+#   # zones                         = var.zones
+# }
+
 resource "azurerm_api_management" "api_management" {
   name                = azurecaf_name.api_management_name.result
   location            = var.location
@@ -22,7 +51,7 @@ resource "azurerm_api_management" "api_management" {
   publisher_name      = var.publisher_name
   sku_name            = "${var.sku_name}_${var.sku_capacity}"
   identity {
-    type         = "SystemAssigned, UserAssigned"
+    type         = "UserAssigned"
     identity_ids = [var.user_assigned_identity_id]
   }
   virtual_network_type = "External"
@@ -39,15 +68,15 @@ resource "azurerm_api_management" "api_management" {
   }
 }
 
-data "azurerm_resource_group" "resource_group" {
-  name = var.resource_group_name
-}
-
-# resource "azurerm_role_assignment" "api_management_system_assigned_managed_identity_cognitive_services_openai_user" {
-#   scope                = data.azurerm_resource_group.resource_group.id
-#   role_definition_name = "Cognitive Services OpenAI User"
-#   principal_id         = azurerm_api_management.api_management.identity.*.principal_id[0]
+# data "azurerm_resource_group" "resource_group" {
+#   name = var.resource_group_name
 # }
+
+# # resource "azurerm_role_assignment" "api_management_system_assigned_managed_identity_cognitive_services_openai_user" {
+# #   scope                = data.azurerm_resource_group.resource_group.id
+# #   role_definition_name = "Cognitive Services OpenAI User"
+# #   principal_id         = azurerm_api_management.api_management.identity.*.principal_id[0]
+# # }
 
 resource "azurerm_monitor_diagnostic_setting" "apim_logging" {
   name                       = "apim-logging"
@@ -73,7 +102,6 @@ module "private_endpoint" {
   location                       = var.location
   subnet_id                      = var.private_endpoint_subnet_id
   subresource_names              = ["Gateway"]
-  is_manual_connection           = false
 }
 
 resource "azapi_update_resource" "update_api_management" {
@@ -83,6 +111,10 @@ resource "azapi_update_resource" "update_api_management" {
   body = {
     properties = {
       publicNetworkAccess = "Disabled"
+      # virtualNetworkConfiguration = {
+      #   subnetResourceId = var.api_management_subnet_id
+      # }
+      # virtualNetworkType = "External"
     }
   }
 }
